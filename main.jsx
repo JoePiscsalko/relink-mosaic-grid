cf5dd99ed95c0ef7d9e422cb97561022a81c31bd
JoePiscsalko <jpiscsalko@relinkmedical.com>
2026-08-19 11:32:13 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Shell from "./Shell.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Shell />
-  </React.StrictMode>
-);
