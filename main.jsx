0a242ce4edc74f0d1bbb9bf6a6eb68027dfe7c6e
JoePiscsalko <jpiscsalko@relinkmedical.com>
2026-08-19 08:41:35 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Shell from "./Shell.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Shell />
+  </React.StrictMode>
+);
